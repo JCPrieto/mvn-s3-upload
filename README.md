f04479359fe044a073eb23ed80c51f5275903c76
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2020-05-30 13:34:39 +0200

    Subida de la lógica (sin testar)

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # mvn-s3-upload
 Plugin de Maven para subir empaquetados a un bucket S3
+
+Proyecto en curso....
+
+Ejemplo de uso:
+
+```
+<plugins>
+    <plugin>
+        <artifactId>mvn-s3-upload</artifactId>
+        <version>0.0.1</version>
+        <configuration>
+            <bucket>my-bucket</bucket>
+            <region>eu-west-1</region>
+            <path>folder1/folder2/</path>
+            <accessKey>XXXXXXXXXXX</accessKey>
+            <secretKey>XXXXXXXXXXX</secretKey>
+            <cannonicalIds>
+                <cannonicalId>XXXXXXXXXXX</cannonicalId>
+                <cannonicalId>XXXXXXXXXXX</cannonicalId>
+                <cannonicalId>XXXXXXXXXXX</cannonicalId>
+            </cannonicalIds>
+        </configuration>
+        <executions>
+            <execution>
+                <goals>
+                    <goal>s3uploader</goal>
+                </goals>
+            </execution>
+        </executions>
+    </plugin>
+</plugins>
+```
+
+ToDo
--- src/main/java/es/jklabs/mvn/S3Uploader.java
+++ src/main/java/es/jklabs/mvn/S3Uploader.java
@@ -1,16 +1,70 @@
 package es.jklabs.mvn;
 
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.CanonicalGrantee;
+import com.amazonaws.services.s3.model.Permission;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.util.Arrays;
 
 @Mojo(name = "s3uploader", defaultPhase = LifecyclePhase.DEPLOY)
 public class S3Uploader extends AbstractMojo {
 
+    @Parameter(property = "project", readonly = true)
+    private MavenProject project;
+
+    @Parameter(property = "aws.s3.bucket", required = true)
+    private String bucket;
+
+    @Parameter(property = "aws.s3.region", required = true)
+    private String region;
+
+    @Parameter(property = "aws.s3.path", required = true)
+    private String path;
+
+    @Parameter(property = "aws.s3.accesskey", required = true)
+    private String accessKey;
+
+    @Parameter(property = "aws.s3.secretkey", required = true)
+    private String secretKey;
+
+    @Parameter(property = "aws.s3.cannonicalIds")
+    private String[] cannonicalIds;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
-        //ToDo
+        File file = project.getArtifact().getFile();
+        upload(file);
+    }
+
+    private void upload(File file) {
+        AmazonS3 s3 = getAmazonS3();
+        PutObjectRequest request = new PutObjectRequest(bucket, path + file.getName(), file);
+        if (s3.putObject(request) != null) {
+            AccessControlList acl = s3.getObjectAcl(bucket, path + file.getName());
+            Arrays.stream(cannonicalIds).forEach(c ->
+                    acl.grantPermission(new CanonicalGrantee(c), Permission.Read));
+            s3.setObjectAcl(bucket, path + file.getName(), acl);
+        }
+    }
+
+    private AmazonS3 getAmazonS3() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(region)
+                .build();
     }
 }
